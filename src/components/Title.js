e9cf2c7b297d049e0507ac78aab0ea94498719a3
LukaszWiszniewski <wisznu07@gmail.com>
2018-02-28 14:21:17 +0100

    Added title class

---
+++ src/components/Title.js
@@ -0,0 +1,21 @@
+class Title extends React.Component{
+    constructor (props){
+        super(props);
+        titleName: 'ToDo list';
+    }
+    static defaultProps ={
+        numberOfItems: 0
+    }
+    static propTypes = {
+        numberOfItems:React.PropTypes.number.isRequired
+    }
+    render(){
+        return(
+            <div className = "titlecontainer">
+                <div className = "title">{this.state.titleName}</div>
+                <div className = "numberOfItems">{this.props.numberOfItems}</div>
+            </div>
+        )
+    }
+
+}
